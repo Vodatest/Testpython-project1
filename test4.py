0f94551f8aef78260ddb7ffa78e8cb6cf0b82cc2
Vodatest <116036346+Vodatest@users.noreply.github.com>
2022-10-19 17:42:23 -0400

    Update test4.py

--- test4.py
+++ test4.py
@@ -1,10 +1,7 @@
-
 # Comparison and assignment operators
-print(5 < 4)
-
+print(5 < 3)
 # Membership operator
 if 5 in [1, 2, 3, 4, 5]:
     print('is contained')
 else:
     print('is not contained')
-	
