7c89cf0bfe58dc0d3bb9a934a5de29ae133f3b2b
Vodatest <116036346+Vodatest@users.noreply.github.com>
2022-10-19 16:54:11 -0400

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,16 +1,17 @@
 """
 high level support for doing this and that.
 """
-def quadratic(a1, b1, c1, x1):
-    
-    """
-    high level support for doing this and that.
-    """
-    
-    # Calculate solution to a quadratic equation using the quadratic
-    # formula.
-    #
-    # A quadratic equation always has 2 solutions x_1 and x_2.
-    x_1 = (- b1+(b1**2-4*a1*c1)**(1/2)) / (2*a1)
-    x_2 = (- b1-(b1**2-4*a1*c1)**(1/2)) / (2*a1)
-    return x_1, x_2
+# Aligned with opening delimiter.
+foo = long_function_name(var_one, var_two,
+                         var_three, var_four)
+
+# Add 4 spaces (an extra level of indentation) to distinguish arguments from the rest.
+def long_function_name(
+        var_one, var_two, var_three,
+        var_four):
+    print(var_one)
+
+# Hanging indents should add a level.
+foo = long_function_name(
+    var_one, var_two,
+    var_three, var_four)
