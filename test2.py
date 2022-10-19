b91a978a7766ec5b61af32fb725d0286251b01d3
Vodatest <116036346+Vodatest@users.noreply.github.com>
2022-10-19 17:27:08 -0400

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,14 +1,10 @@
-"""
-high level support for doing this and that.
-"""
-def quadratic(A, B, C):
-    """
-    high level support for doing this and that.
-    """
-    # Calculate solution to a quadratic equation using the quadratic
-    # formula.
-    #
-    # A quadratic equation always has 2 solutions x_1 and x_2.
-    X_1 = (- B+(B**2-4*A*C)**(1/2)) / (2*A)
-    X_2 = (- B-(B**2-4*A*C)**(1/2)) / (2*A)
-    return X_1, X_2
+
+# pylint: disable=C0103
+# pylint: disable=C0114
+# multi-line statement
+
+def multiply(
+        arg1, arg2, arg3,
+        arg4, arg5):
+    """The function multiplies five numbers"""
+    return arg1 * arg2 * arg3 * arg4 * arg5
