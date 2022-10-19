13824e9b03080bebce28242a9105f3b2960a981f
Vodatest <116036346+Vodatest@users.noreply.github.com>
2022-10-19 17:31:06 -0400

    Create test3.py

---
+++ test3.py
@@ -0,0 +1,10 @@
+
+# pylint: disable=C0103
+# pylint: disable=C0114
+# check for prefixes and suffixes
+
+name = 'Munich'
+if name.startswith('M'):
+    print('Name starts with M')
+if name.endswith('ing'):
+    print('Name ends with ing')
