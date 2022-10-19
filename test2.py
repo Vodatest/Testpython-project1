579dce77b8a8da171b7591d3c1e1234f37124faa
Vodatest <116036346+Vodatest@users.noreply.github.com>
2022-10-19 17:02:45 -0400

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,17 +1,14 @@
 """
 high level support for doing this and that.
 """
-# Aligned with opening delimiter.
-foo = long_function_name(var_one, var_two,
-                         var_three, var_four)
-
-# Add 4 spaces (an extra level of indentation) to distinguish arguments from the rest.
-def long_function_name(
-        var_one, var_two, var_three,
-        var_four):
-    print(var_one)
-
-# Hanging indents should add a level.
-foo = long_function_name(
-    var_one, var_two,
-    var_three, var_four)
+def quadratic(a, b, c, x):
+    """
+    high level support for doing this and that.
+    """
+    # Calculate solution to a quadratic equation using the quadratic
+    # formula.
+    #
+    # A quadratic equation always has 2 solutions x_1 and x_2.
+    x_1 = (- b+(b**2-4*a*c)**(1/2)) / (2*a)
+    x_2 = (- b-(b**2-4*a*c)**(1/2)) / (2*a)
+    return x_1, x_2
