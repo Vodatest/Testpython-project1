e9df7fc35e72faa4f4cbdfe2b6599d3dac301ae4
Vodatest <116036346+Vodatest@users.noreply.github.com>
2022-10-19 16:47:53 -0400

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,7 +1,7 @@
 """
 high level support for doing this and that.
 """
-def quadratic(a, b, c, x):
+def quadratic(a1, b1, c1, x1):
     
     """
     high level support for doing this and that.
@@ -11,6 +11,6 @@ def quadratic(a, b, c, x):
     # formula.
     #
     # A quadratic equation always has 2 solutions x_1 and x_2.
-    x_1 = (- b+(b**2-4*a*c)**(1/2)) / (2*a)
-    x_2 = (- b-(b**2-4*a*c)**(1/2)) / (2*a)
+    x_1 = (- b1+(b1**2-4*a1*c1)**(1/2)) / (2*a1)
+    x_2 = (- b1-(b1**2-4*a1*c1)**(1/2)) / (2*a1)
     return x_1, x_2
