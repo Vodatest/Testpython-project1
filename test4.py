73e38b9fd46838f8b3749b36888b665a3538c160
Vodatest <116036346+Vodatest@users.noreply.github.com>
2022-10-19 17:36:16 -0400

    Update test4.py

--- test4.py
+++ test4.py
@@ -1,16 +1,10 @@
 
-# pylint: disable=C0103
-# pylint: disable=C0114
-# exceptions
+# Comparison and assignment operators
+print(5 < 4)
 
-def int2hex(integer):
-    """Function that converts an integer to a hexadecimal string"""
-    try:
-        hexadecimal = hex(integer)
-        return hexadecimal
-
-    except TypeError:
-        print('An integer should be provided as input')
-
-# call the function
-int2hex(5)
+# Membership operator
+if 5 in [1, 2, 3, 4, 5]:
+    print('is contained')
+else:
+    print('is not contained')
+	
