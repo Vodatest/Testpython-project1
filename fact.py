7cf062c6391cf801f6cfd0c11529e77f574d8a2b
Vodatest <116036346+Vodatest@users.noreply.github.com>
2022-10-19 07:07:34 -0400

    Update fact.py

--- fact.py
+++ fact.py
@@ -1,5 +1,12 @@
-def factorial(n):
-    if n == 0 or n == 1:
+"""
+This module has the method to calculate factorial of the given number
+"""
+def factorial(num):
+    """ Calculates factorial of the passed number
+    num (int): Integer for which factorial is requested
+    returns (int): Returns Factorial
+    """
+    if num in (0, 1):
         return 1
-    return n * factorial(n-1)
+    return num * factorial(num-1)
 print(factorial(5))
